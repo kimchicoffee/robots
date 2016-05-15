b0aeb8cba34a9e1d22d84c0c29f99de3b99d1512
Sujin Lee <pasdesujin@gmail.com>
2016-05-15 01:31:53 -0400

    Implement toggling report abuse.

--- components/Comment.js
+++ components/Comment.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 
 export default class Comment extends Component {
+	constructor() {
+		super()
+		this.state = { isAbusive: false }
+	}
+
 	render() {
+		let commentText = this.state.isAbusive ? <em> Content marked as abusive </em> : this.props.text
+		let reportLabel = this.state.isAbusive ? 'Withdral Report' : 'Report as Abuse'
+		
 		return (
 			<article className = 'comment'>
 				<p className = 'comment-header'> {this.props.author} </p>
-				<p className = 'comment-body'> {this.props.text} </p>
+				<p className = 'comment-body'> { commentText } </p>
+				<div className = "comment-actions">
+					<a href="#" onClick={ this.toggleAbuse.bind(this) }> { reportLabel }</a>
+				</div>
 			</article>
 			)
+	}
+
+	toggleAbuse(event) {
+		event.preventDefault()
+		this.setState({ isAbusive: !(this.state.isAbusive) })
 	}
 }
