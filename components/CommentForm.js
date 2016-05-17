03505163451ced9c70731e2bcd7dd6df0e07e047
Sujin Lee <pasdesujin@gmail.com>
2016-05-17 15:49:25 -0400

    Implement getCharacter counts of comment text

--- components/CommentForm.js
+++ components/CommentForm.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 
 export default class CommentForm extends Component {
+	constructor() {
+		super();
+		this.state = {
+			characters : 0
+		}
+	}
+
 	render() {
 		return(
 			<form className = "comment-form" onSubmit = { this.handleSubmit.bind(this) }>
 				<label>New comment</label>
 				<div className ="comment-form-fields">
 					<input placeholder = "Name:" ref = {(input) => this.author = input} />
-					<textarea placeholder="Comment:" ref = {(input) => this.text = input } /> 
+					<textarea placeholder="Comment:" ref = {(input) => this.text = input } onChange = { this.getCharacters.bind(this) }/> 
 				</div>
+				<p> { this.state.characters } characters </p>
 				<div className = "comment-form-actions">
 					<button type ='submit'>
 						Post Comment
@@ -18,6 +26,12 @@ export default class CommentForm extends Component {
 			)
 	}
 
+	getCharacters() {
+		this.setState({
+			characters: this.text.value.length
+		})
+	}
+
 	handleSubmit(event) {
 		event.preventDefault();
 
@@ -25,5 +39,9 @@ export default class CommentForm extends Component {
 
 		this.author.value = ''
 		this.text.value = ''
+
+		this.setState({ 
+			characters: 0
+		})
 	}
 }
